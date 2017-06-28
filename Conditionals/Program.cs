413ab6de763e94afd2b12789c2a8d79ef124a05b
Woodsmike <Woodsmike@users.noreply.github.com>
2017-06-28 11:56:32 -0400

    revised1

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,7 +10,25 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("How many people in your party?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter in your total bill.");
+            double totalBill = double.Parse(Console.ReadLine());
+            
+            
+            double moreDiscountTotal = (totalBill - (totalBill * .10d));
+            double lessDiscountTotal = (totalBill - (totalBill * .05d));
 
+            if (totalBill >= 50d)
+            {
+                Console.WriteLine("Total with discount: $" + moreDiscountTotal);
+            }
+            else
+            {
+                Console.WriteLine("Total with discount: $" + lessDiscountTotal);
+            }
+
+           
         }
     }
 }
