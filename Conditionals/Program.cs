6915952a77e7ddebb0c59f2fbee2d712a47c4688
Woodsmike <Woodsmike@users.noreply.github.com>
2017-06-28 13:45:05 -0400

    revised with restaurant

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -11,26 +11,33 @@ namespace Conditionals
         static void Main(string[] args)
         {
             //have the user enter amount of people and their total bill
-            Console.WriteLine("How many people in your party?");
+            Console.WriteLine("Thank you for dining at our restaurant?");
+            Console.WriteLine("How many people are in your party?");
             int numberOfPeople = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Please enter in your total bill.");
             double totalBill = double.Parse(Console.ReadLine());
             
             //set variables for the discount depending on the amount of total bill
             double moreDiscountTotal = (totalBill - (totalBill * .10d));
             double lessDiscountTotal = (totalBill - (totalBill * .05d));
+            double billSplit;
 
             //figure the discounted amount for the total bill
             if (totalBill >= 50d)
             {
-                Console.WriteLine("Total with discount: $" + moreDiscountTotal);
+                Console.WriteLine("You received a 10% discount.");
+                Console.WriteLine("Total with your discount: $" + moreDiscountTotal);
+                billSplit = moreDiscountTotal / numberOfPeople;
+                Console.WriteLine("Each person in your party will pay $" + billSplit);
             }
             else
             {
-                Console.WriteLine("Total with discount: $" + lessDiscountTotal);
+                Console.WriteLine("You received a 5% discount.");
+                Console.WriteLine("Total with your discount: $" + lessDiscountTotal);
+                billSplit = lessDiscountTotal / numberOfPeople;
+                Console.WriteLine("Each person in your party will pay $" + billSplit);
             }
-
-           
         }
     }
 }
